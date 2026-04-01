15cbb9a95dfc7b5fa5345e5ec2d98a075c7947c8
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-04-01 22:10:30 +0200

    feat: added form group data

--- src/app/employee/edit/employee-edit/employee-edit.component.ts
+++ src/app/employee/edit/employee-edit/employee-edit.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { EmployeeService } from '../../../services/employee.services';
+import { Employee } from '../../../models/employee.model';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-edit',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './employee-edit.component.html',
-  styleUrl: './employee-edit.component.scss'
+  styleUrl: './employee-edit.component.scss',
 })
-export class EmployeeEditComponent {
+export class EmployeeEditComponent implements OnInit {
+  private employeeServices = inject(EmployeeService);
+  private fb = inject(FormBuilder);
 
+  employee! : Employee;
+  employeeForm! : FormGroup;
+
+  ngOnInit(): void{
+    this.employeeServices.getEmployee(1).subscribe({
+      next: (data) => {
+        this.employee = data; 
+        this.employeeForm = this.fb.group({
+          name: [data.name],
+          email: [data.email],
+          department: [data.department];
+        })
+      },
+      error: (err) => {
+        console.log(err); 
+      }
+    })
+  }
 }
